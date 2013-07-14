27928a24c4a1b1cb6af6273b3236050781184bbc
Jaliborc <contact@jaliborc.com>
2013-07-14 16:25:35 +0100

    Added color highlight options

--- general.lua
+++ general.lua
@@ -11,29 +11,42 @@ local frame
 local Managed = General:NewCheckButton(L.ActPanel, L.ActPanelTip)
 Managed:SetPoint('TOPLEFT', 15, -120)
 Managed:SetScript('OnClick', function()
-  if frame.sets.position then
-    frame:SavePosition(nil)
-  else
-    frame:SavePosition(frame:GetPoint())
-  end
+    if frame.sets.position then
+        frame:SavePosition(nil)
+    else
+        frame:SavePosition(frame:GetPoint())
+    end
 end)
 
 local Reversed = General:NewCheckButton(L.LeftFilters, L.LeftFiltersTip)
 Reversed:SetPoint('TOPLEFT', Managed, 'BOTTOMLEFT', 0, -5)
 Reversed:SetScript('OnClick', function()
-  frame:SetLeftSideFilter(not frame:IsSideFilterOnLeft())
+    frame:SetLeftSideFilter(not frame:IsSideFilterOnLeft())
 end)
 
+local anchor = Reversed
+for _, name in ipairs {'ItemsByQuality', 'UnusableItems', 'SetItems', 'QuestItems'} do
+    local button = General:NewCheckButton(L['Highlight' .. name])
+    button:SetPoint('TOPLEFT', anchor, 'BOTTOMLEFT', 0, -5)
+    button:SetChecked(not Combuctor:GetSetting('disable' .. name))
+    button:SetScript('OnClick', function()
+        Combuctor:ToggleSetting('disable' .. name)
+        Combuctor:UpdateFrames()
+    end)
+
+    anchor = button
+end
+
 local TipCounts = General:NewCheckButton(L.TipCounts, L.TipCountsTip)
-TipCounts:SetPoint('TOPLEFT', Reversed, 'BOTTOMLEFT', 0, -5)
+TipCounts:SetPoint('TOPLEFT', anchor, 'BOTTOMLEFT', 0, -5)
 TipCounts:SetChecked(Combuctor:IsTipCountEnabled())
 TipCounts:SetScript('OnClick', function()
-  Combuctor:ToggleTipCounts()
+    Combuctor:ToggleTipCounts()
 end)
 
 function General:OnFrameChanged()
-  frame = Combuctor:GetFrame(Addon.frame)
+    frame = Combuctor:GetFrame(Addon.frame)
 
-  Managed:SetChecked(not frame.sets.position)
-  Reversed:SetChecked(frame:IsSideFilterOnLeft())
+    Managed:SetChecked(not frame.sets.position)
+    Reversed:SetChecked(frame:IsSideFilterOnLeft())
 end
